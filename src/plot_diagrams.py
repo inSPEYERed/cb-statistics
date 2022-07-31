28f8c893da9f52d5d998de43b36da92b915f6402
Splines <dominic-plein@gmx.de>
2022-07-31 17:49:08 +0200

    Save plots to PDF

--- src/plot_diagrams.py
+++ src/plot_diagrams.py
@@ -4,6 +4,7 @@ from matplotlib import ticker
 
 # COLORS = ['tab:orange', 'tab:blue']
 COLORS = ['#057976', '#BF2118', '#014485']
+# See this if hatches (patterns) are not showing up: https://stackoverflow.com/a/59389823
 PATTERNS = ['.', '/', '.O']
 
 # https://stackoverflow.com/a/39566040/9655481
@@ -36,7 +37,7 @@ def plot_bookings_per_week(per_week_results: dict[int, list[int]]) -> None:
         year_counts = per_week_results[year]
 
         rects = plt.bar(x_values - (bar_width/2) + i*bar_width,
-                        year_counts, bar_width, label=year, color=COLORS[i], hatch=PATTERNS[i])
+                        year_counts, bar_width, label=year, color=COLORS[i], hatch=PATTERNS[i], alpha=0.99)
         # ax.bar_label(rects, padding=3)
 
     plt.xlabel('Kalenderwoche (KW)')
@@ -48,7 +49,8 @@ def plot_bookings_per_week(per_week_results: dict[int, list[int]]) -> None:
     ax.grid(axis='y')
 
     plt.tight_layout()
-    plt.show()
+    # plt.show()
+    plt.savefig('./plots/per-week.pdf')
 
 
 def plot_bookings_per_weekday(per_weekday_results: dict[int, list[int]]) -> None:
@@ -66,8 +68,8 @@ def plot_bookings_per_weekday(per_weekday_results: dict[int, list[int]]) -> None
         year_counts = per_weekday_results[year]
 
         rects = plt.bar(x_values - (bar_width/2) + i*bar_width,
-                        year_counts, bar_width, label=year, color=COLORS[i], hatch=PATTERNS[i])
-        # ax.bar_label(rects, padding=3)
+                        year_counts, bar_width, label=year, color=COLORS[i], hatch=PATTERNS[i], alpha=0.99)
+        ax.bar_label(rects, padding=3)
 
     plt.xlabel('Wochentag')
     plt.ylabel('Anzahl Ausleihen')
@@ -82,7 +84,8 @@ def plot_bookings_per_weekday(per_weekday_results: dict[int, list[int]]) -> None
     ax.grid(axis='y')
 
     plt.tight_layout()
-    plt.show()
+    # plt.show()
+    plt.savefig('./plots/per-weekday.pdf')
 
 
 def plot_bookings_per_item(per_item_results: dict[int, dict[str, int]]) -> None:
@@ -100,16 +103,13 @@ def plot_bookings_per_item(per_item_results: dict[int, dict[str, int]]) -> None:
     for i, year in enumerate(sorted(per_item_results)):
         year_results = per_item_results[year]
         values = list(year_results.values())
-        print('VALUES')
-        print(values)
-
         if i == 0:
             item_names = year_results.keys()
             x_ticklabels.extend(item_names)
 
         rects = plt.bar(x_values - (bar_width/2) + i*bar_width,
-                        values, bar_width, label=year, color=COLORS[i], hatch=PATTERNS[i])
-        # ax.bar_label(rects, padding=3)
+                        values, bar_width, label=year, color=COLORS[i], hatch=PATTERNS[i], alpha=0.99)
+        ax.bar_label(rects, padding=3)
 
     plt.xlabel('Lastenrad')
     plt.ylabel('Anzahl Ausleihen')
@@ -122,4 +122,5 @@ def plot_bookings_per_item(per_item_results: dict[int, dict[str, int]]) -> None:
     ax.grid(axis='y')
 
     plt.tight_layout()
-    plt.show()
+    # plt.show()
+    plt.savefig('./plots/per-item.pdf')
